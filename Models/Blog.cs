537a2f5c1e7bf087c96bd9d697904e0a90160dad
YulinChen <yolin1111@gmail.com>
2020-09-25 23:37:40 +0800

    first commit

---
+++ Models/Blog.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace SampleBlog.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public int Rating { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ Models/BlogContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleBlog.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() { }
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Initial Catalog=Blog;User Id=sa;Password=sasa");
+        }
+
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+    }
+}
