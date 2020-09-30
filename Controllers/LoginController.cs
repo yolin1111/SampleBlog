7b4a66f58ea761582bc89d3a42a86138a362695e
YulinChen <yolin1111@gmail.com>
2020-09-30 17:21:34 +0800

    練習登入功能

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using SampleBlog.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace SampleBlog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // POST api/<LoginController>
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Login(Login login)
+        {
+            //TODO 呼叫市府SSO，需要申請，webservice wsdl
+            if (login.UserName == "Yulin" && login.PassWord == "1234")
+            {
+                return Ok("登入成功");
+            }
+            return BadRequest("登入失敗");
+        }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,9 @@
+namespace SampleBlog.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+}
